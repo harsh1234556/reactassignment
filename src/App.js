27b9a8a16b25818c6645945c6738d35d11db314a
harsh1234556 <87651780+harsh1234556@users.noreply.github.com>
2021-08-31 14:17:16 +0530

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import  classes  from './index.css';
+
+function App() {
+  return( 
+    
+    <div className={classes.container}>
+    <div className="topleftpane"><img    src={"https://img.icons8.com/color/2x/polishing-cloth.png"} alt="process"></img><div className="topleftpanetext">Competetive Analysis <div>
+    <div className="topleftpanepara"> See how others solve similar problems and try to not reinvent the wheel<br /></div><a href="www.google.com">Read More</a></div></div></div>
+     <div className="topmidpane"><img  src={"https://img.icons8.com/color/2x/polishing-cloth.png"} alt="process"></img><div className="topleftpanetext">Data analysis <div>
+    <div className="topleftpanepara"> See how others solve similar problems and try to not reinvent the wheel<br /></div><a href="www.google.com">Read More</a></div></div></div> 
+    <div className="toprightpane"><img  src={"https://img.icons8.com/color/2x/polishing-cloth.png"} alt="process"></img><div className="topleftpanetext">User feedback <div>
+    <div className="topleftpanepara"> See how others solve similar problems and try to not reinvent the wheel<br /></div><a href="www.google.com">Read More</a></div></div></div>
+    <div className="midleftpane"><img  src={"https://img.icons8.com/color/2x/polishing-cloth.png"} alt="process"></img><div className="topleftpanetext">User stories<div>
+    <div className="topleftpanepara"> See how others solve similar problems and try to not reinvent the wheel<br /></div><a href="www.google.com">Read More</a></div></div></div>
+    <div className="midmidpane"><img  src={"https://img.icons8.com/color/2x/polishing-cloth.png"} alt="process"></img><div className="topleftpanetext">User flows<div>
+    <div className="topleftpanepara"> See how others solve similar problems and try to not reinvent the wheel<br /></div><a href="www.google.com">Read More</a></div></div></div>
+    <div className="midrightpane"><img  src={"https://img.icons8.com/color/2x/polishing-cloth.png"} alt="process"></img><div className="topleftpanetext">Red routes<div>
+    <div className="topleftpanepara"> See how others solve similar problems and try to not reinvent the wheel<br /></div><a href="www.google.com">Read More</a></div></div></div>
+    <div className="downleftpane"><img  src={"https://img.icons8.com/color/2x/polishing-cloth.png"} alt="process"></img><div className="topleftpanetext">Brainstorm  & sketch <div>
+    <div className="topleftpanepara"> See how others solve similar problems and try to not reinvent the wheel<br /></div><a href="www.google.com">Read More</a></div></div></div>
+     <div className="downmidpane"><img  src={"https://img.icons8.com/color/2x/polishing-cloth.png"} alt="process"></img><div className="topleftpanetext">Wireframe <div>
+    <div className="topleftpanepara"> See how others solve similar problems and try to not reinvent the wheel<br /></div><a href="www.google.com">Read More</a></div></div></div> 
+     <div className="downrightpane"><img  src={"https://img.icons8.com/color/2x/polishing-cloth.png"} alt="process"></img><div className="topleftpanetext">Prototype<div>
+    <div className="topleftpanepara"> See how others solve similar problems and try to not reinvent the wheel<br /></div><a href="www.google.com">Read More</a></div></div></div>
+  </div>
+
+
+
+  );
+}
+
+export default App;
